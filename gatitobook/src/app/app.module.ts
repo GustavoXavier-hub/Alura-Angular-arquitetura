0838395b730a9b9ed059b80555e40cf26c237b5b
Gustavo Xavier <gus.guilhermexavier08@gmail.com>
2022-08-11 21:14:48 -0300

    Formulario Template-Driven com erro

--- gatitobook/src/app/app.module.ts
+++ gatitobook/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations:[AppComponent],
-  imports:[BrowserModule,AppRoutingModule],
+  imports:[BrowserModule,AppRoutingModule,FormsModule],
   providers:[],
   bootstrap:[AppComponent],
 })
